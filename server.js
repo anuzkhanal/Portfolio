f8ea78f338bd206e65b01281db207eeeb812c1a5
Anuj Khanal <anuz.khanal@gmail.com>
2022-03-16 14:55:00 +0200

    server file

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
-const contactRoute = require("./route/contactRoute");
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const contactRoute = require('./route/contactRoute')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.use("/", contactRoute);
+app.use('/', contactRoute)
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('app/client/build'))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
 }
 
-const port = process.env.PORT || 8000;
-app.listen(port, console.log(`server listing to port ${port}`));
+const port = process.env.PORT || 8000
+app.listen(port, console.log(`server listing to port ${port}`))
